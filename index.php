28a79bde51a5bcc916a98af1e0722fe6b5df484a
mbejda <mbejda@live.com>
2013-09-17 00:15:10 -0400

    added enq

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
-echo "test";
+class plugin {
+	function __construct()
+	{
+    add_action( 'wp_enqueue_scripts', 'wp_enqueue_scripts' );
+    add_action( 'admin_enqueue_scripts', 'admin_enqueue_scripts' );
 
+	}
+	private function wp_enqueue_scripts()
+	{
+		wp_enqueue_script( 'jquery');
+		wp_enqueue_script( 'main', plugins_url( 'js/script.js', __FILE__), array('jquery'), $ver = time(), $in_footer = true )
+	}
+	private function admin_enqueue_scripts()
+	{
+		wp_enqueue_script( 'jquery');
+		wp_enqueue_script( 'admin', plugins_url( 'js/admin.js', __FILE__), array('jquery'), $ver = time(), $in_footer = true )
+
+	}
+
+}
 ?>
