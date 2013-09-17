2ec792e7b896ca1175b4293160c2f78b2198b855
mbejda <mbejda@live.com>
2013-09-17 00:34:31 -0400

    changes

--- index.php
+++ index.php
@@ -2,8 +2,29 @@
 class plugin {
 	function __construct()
 	{
-    add_action( 'wp_enqueue_scripts', 'wp_enqueue_scripts' );
-    add_action( 'admin_enqueue_scripts', 'admin_enqueue_scripts' );
+  add_action('init',array(&$this,'init'));
+  if ( is_admin() ){ // admin actions
+   add_action( 'admin_menu', array('admin_menu' ));
+   add_action( 'admin_init', array(&$this,'admin_init' ));
+   add_action( 'admin_enqueue_scripts', array(&$this,'admin_enqueue_scripts' ));
+
+} else{
+    add_action( 'wp_enqueue_scripts', array(&$this,'wp_enqueue_scripts' ));
+
+
+}
+
+	}
+	private function init()
+	{
+
+	}
+	private function admin_init()
+	{
+
+	}
+	private function admin_menu()
+	{
 
 	}
 	private function wp_enqueue_scripts()
